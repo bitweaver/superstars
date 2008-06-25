8e356c8725fc49cf21f8ae5507a469e9a7105bbb
Christian Fowler <spider@viovio.com>
2008-06-25 22:04:40 +0000

    change display call to use a hash for display options, add display_mode to options hash

--- details.php
+++ details.php
@@ -1,9 +1,9 @@
 <?php
 /**
- * $Header: /cvsroot/bitweaver/_bit_superstars/details.php,v 1.3 2008/06/19 09:34:24 lsces Exp $
+ * $Header: /cvsroot/bitweaver/_bit_superstars/details.php,v 1.4 2008/06/25 22:04:40 spiderr Exp $
  * date created 2006/02/10
  * @author xing <xing@synapse.plus.com>
- * @version $Revision: 1.3 $ $Date: 2008/06/19 09:34:24 $
+ * @version $Revision: 1.4 $ $Date: 2008/06/25 22:04:40 $
  * @package superstars
  * @subpackage functions
  */
@@ -24,5 +24,5 @@ $stars = new LibertyStars( $_REQUEST['content_id'] );
 $stars->getRatingDetails( !empty( $_REQUEST['show_raters'] ) );
 
 $gBitSmarty->assign( 'starsDetails', $stars->mInfo );
-$gBitSystem->display( 'bitpackage:stars/details.tpl', tra( 'Details of Rated Content' ) );
+$gBitSystem->display( 'bitpackage:stars/details.tpl', tra( 'Details of Rated Content' ) , array( 'display_mode' => 'display' ));
 ?>
